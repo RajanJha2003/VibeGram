178e673b18a8e210294eb18583928f4dd19e64bd
Rajan <rajanj448@student.sfit.ac.in>
2024-09-12 11:49:19 +0530

    Connect with MongoDB Database

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,26 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 
-const corsOptions={
-    origin:"http://localhost:5173/",
-    credentials:true
-}
+const corsOptions = {
+  origin: "http://localhost:5173/",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
-
-app.listen(PORT,()=>{
-    console.log(`Server listen at port ${PORT}`);
-})
-
-
-
-
+app.listen(PORT, () => {
+    connectDB();
+  console.log(`Server listen at port ${PORT}`);
+});
